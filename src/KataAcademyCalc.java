6890a12b12d460ee2f8bdb336e7bd70c790ceeb9
Kirill <vkashe1337@gmail.com>
2023-11-21 09:53:49 +0300

    Added element in StringBuilder.

--- src/KataAcademyCalc.java
+++ src/KataAcademyCalc.java
@@ -73,7 +73,7 @@ public class KataAcademyCalc {
 class Roman {
     public static String[] arrayToRoman = new String[]{"0", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII",
             "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX", "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX",
-            "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII",
+            "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX","XL", "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII",
             "XLVIII", "XLIX", "L"};
 
     public static int toArabic(String roman) {
